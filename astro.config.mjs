b25de3cc5dd3fb1eb963ab4bd60123f71454ca88
abjunior92 <junior.berselli@gmail.com>
2024-04-05 10:09:49 +0200

    add favicon; other fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,13 +9,23 @@ const options = {
   keepBackground: false
 };
 
+const LOCALHOST_URL = "http://localhost:4321";
+const LIVE_URL = "https://abjunior92.github.io";
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
+
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     syntaxHighlight: false,
     rehypePlugins: [[rehypePrettyCode, options]]
   },
-  site: "https://abjunior92.github.io",
+  site: BASE_URL,
   integrations: [mdx(), sitemap(), tailwind(), react()],
   server: {
     host: true
